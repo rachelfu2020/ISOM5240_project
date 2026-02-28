8ce0acdffcb9ecfa174bee10c74921d6a45930a4
Rachel <75394424+rachelfu2020@users.noreply.github.com>
2026-02-28 17:06:47 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -11,8 +11,7 @@ st.header("Title: Age Classification using ViT")
 
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
-age_classifier = pipeline("image-classification",
-                          model="nateraw/vit-age-classifier")
+age_classifier = pipeline("image-classification", model="akashmaggon/vit-base-age-classification")
 
 image_name = "middleagedMan.jpg"
 image_name = Image.open(image_name).convert("RGB")
