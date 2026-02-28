65c56c6d7ad0a993b001661e7554ec2590037bf6
Rachel <75394424+rachelfu2020@users.noreply.github.com>
2026-02-28 16:51:06 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -6,6 +6,8 @@ import streamlit as st
 
 # Streamlit UI
 print("Title: Age Classification using ViT")
+#Since this is a steamlit application, therefore cannot just use 'Print' but using a steamlit function instead to show
+st.header("Title: Age Classification using ViT")
 
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
