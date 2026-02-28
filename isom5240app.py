59ceb98e85ac743d24ec43b5d63829c4bdf50e03
Rachel <75394424+rachelfu2020@users.noreply.github.com>
2026-02-28 17:41:33 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -5,14 +5,14 @@ import streamlit as st
 
 # function parts
 
-def imgClassifier():
+def imgClassifier(imgFilename):
 
         
         # Load the age classification pipeline
         # The code below should be placed in the main part of the program
         age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
         
-        image_name = "middleagedMan.jpg"
+        image_name = imgFilename 
         image_name = Image.open(image_name).convert("RGB")
         
         # Classify age
@@ -36,7 +36,7 @@ def main ():
         # The code below should be placed in the main part of the program
         age_classifier = pipeline("image-classification", model="prithivMLmods/Age-Classification-SigLIP2")
         
-        image_name = "middleagedMan.jpg"
+        image_name = imgFilename
         image_name = Image.open(image_name).convert("RGB")
         
         # Classify age
